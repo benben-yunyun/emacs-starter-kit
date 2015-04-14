23326b8e50a219d00ef159dcfecef11063736ad5
xiang.yuner@gmail.com <xiang.yuner@gmail.com>
2015-04-14 21:38:32 +0800

    add plugin w3m configuration

--- init.el
+++ init.el
@@ -6,6 +6,11 @@
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
+;; getenv: 获得ORG_HOME系统设置的变量
+;; expand-file-name: 将相对路径转为绝对路径
+;; file-directory-p: 判断文件目录是否存在
+;; add-to-list: 添加element到list中
+;; require: 加载相关特性文件
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
@@ -13,6 +18,7 @@
       (require 'org))))
 
 ;; load the starter kit from the `after-init-hook' so all packages are loaded
+;; unless: 检测假，则执行后续代码
 (add-hook 'after-init-hook
  `(lambda ()
     ;; remember this directory
@@ -24,8 +30,10 @@
                                                       (getenv "ORG_HOME"))))
        '(require 'org))
     ;; load up the starter kit
+    ;; org-label-load-file: 从org文件中加载elisp代码
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
+(put 'upcase-region 'disabled nil)
